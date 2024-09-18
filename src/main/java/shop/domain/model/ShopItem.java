ed72fe52f9b79e5af2688d14ae9fda5b94d10cf0
Patryk Piekarz <patrykpiekarz3700@gmail.com>
2024-09-18 14:23:57 +0200

    shopItem start

--- src/main/java/shop/domain/model/ShopItem.java
+++ src/main/java/shop/domain/model/ShopItem.java
@@ -1,4 +1,26 @@
 package shop.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class ShopItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String imageurl;
+
+    @NotBlank(message = "name.missing")
+    private String name;
+
+    private String description;
+
+    @NotBlank(message = "price.missing")
+    private double Price;
 }
